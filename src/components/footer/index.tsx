d328d1874dff455ad934b5de3d9d086e97ab765c
Diego <diegosmtattoo@gmail.com>
2025-07-16 13:18:20 -0300

    feat: footer session

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { FooterContainer } from "./styles";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <div className="logo-and-copyright">
+        <img src="./images/logo_film-footer.png" alt="logo Filme Lab" />
+
+        <p className="copyright">
+          © Copyright 2025 FilmeLab. Todos os direitos reservados.
+        </p>
+      </div>
+      <p className="filmelab-website">www.filmelab.com.br</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
