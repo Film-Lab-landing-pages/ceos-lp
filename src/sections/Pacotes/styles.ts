1bb72dd494ad2734e18dacf0e919d713de72df2e
Laura Segouras <lausegouras@gmail.com>
2025-07-22 12:03:52 -0300

    feat: css adjustments

--- src/sections/Pacotes/styles.ts
+++ src/sections/Pacotes/styles.ts
@@ -1,10 +1,8 @@
-import { Section } from '@/styles/globalStyles'
+import { Card, Section } from '@/styles/globalStyles'
 import styled from 'styled-components'
 
 export const PacotesSection = styled(Section)`
   margin: 5rem 0;
-  /* background: url('/images/bg_section02.png') center center no-repeat;
-  background-size: cover; */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -30,7 +28,7 @@ export const SubtitleWrapper = styled.div`
 
 export const SubtitleMainText = styled.p`
   color: var(--color-white);
-  font-size: 25px;
+  font-size: 2rem;
   text-align: center;
   margin-bottom: 1rem;
   max-width: 1600px;
@@ -40,7 +38,7 @@ export const SubtitleSpan = styled.span`
   color: var(--color-yellow);
   font-weight: bold;
   text-align: center;
-  font-size: 25px;
+  font-size: 2rem;
   text-align: center;
   max-width: 1600px;
 `
@@ -58,6 +56,8 @@ export const Pacote = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+  padding: 1rem;
+  margin: 6rem 0 2rem 0;
 
   &.recomendado {
     border: 2px solid #ffc802;
@@ -67,36 +67,54 @@ export const Pacote = styled.div`
 `
 
 export const Icon = styled.img`
-  width: 64px;
-  height: 64px;
+  width: 9rem;
+  height: 9rem;
 `
 
 export const PacoteTitle = styled.h3`
   color: #ffc802;
-  font-size: 1.5rem;
-  font-weight: bold;
+  font-size: 3.5rem;
   margin: 0;
+`
+export const CardHeader = styled.li`
+  list-style: none;
+  padding: 0.3rem;
+  color: var(--color-white);
+  font-size: 1.5rem;
+  line-height: 1.4;
+  margin: 2rem;
+`
+export const PacotesCard = styled(Card)`
+  height: 30rem;
 `
 
 export const CardItem = styled.li`
   list-style: none;
-  padding: 0.3rem 0;
-  color: #fff;
-  font-size: 0.95rem;
-  line-height: 1.4;
+  padding: 0.3rem;
+  color: var(--color-white);
+  font-size: 1.5rem;
+  margin: 2rem;
+
+  span.check {
+    color: green;
+    font-weight: bold;
+    margin-right: 0.5rem;
+  }
 `
 
 export const CardItemInativo = styled(CardItem)`
-  color: red;
-  text-decoration: line-through;
+  span.cross {
+    color: red;
+    font-weight: bold;
+    margin-right: 0.5rem;
+  }
 `
 
 export const Selo = styled.div`
-  margin-top: 1.5rem;
-  font-weight: bold;
+  margin-top: 3rem;
   color: #ffc802;
-  background-color: rgba(255, 200, 2, 0.1);
   padding: 0.4rem 1rem;
   border-radius: 6px;
   text-align: center;
+  font-size: 3rem;
 `
