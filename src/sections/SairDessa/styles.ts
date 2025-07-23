da19f38c38b215c8582770a7a521adeb29e41ab2
Diego <diegosmtattoo@gmail.com>
2025-07-23 11:23:59 -0300

    feat: sair dessa mid responsiveness

--- src/sections/SairDessa/styles.ts
+++ src/sections/SairDessa/styles.ts
@@ -7,10 +7,12 @@ export const SairDessaSection = styled(Section)`
   padding: 5rem;
   h2 {
     margin: 0 auto 7rem;
+    font-size: 4.75rem;
+    text-align: center;
   }
   .flex-card {
     display: flex;
-    width: 80%;
+    width: 100%;
     margin: 0 auto;
     padding: 4rem 6rem;
     gap: 8%;
@@ -19,9 +21,19 @@ export const SairDessaSection = styled(Section)`
     border: 1px solid var(--color-yellow);
     border-radius: 8px;
     padding: 4rem 2.5rem;
-    font-size: 1.4rem;
+    font-size: 1.25rem;
     line-height: 2.5em;
     text-align: center;
+  }
+  @media (max-width: 1024px) {
+    .flex-card {
+      flex-direction: column;
+      padding: 4rem;
+      gap: 3rem;
+    }
+    h2 {
+      margin: 0 auto 4rem;
+    }
   }
 `;
 
