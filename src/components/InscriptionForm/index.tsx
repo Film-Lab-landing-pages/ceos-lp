270e9afe5d488813fdd74d46e47353ecdeaf5311
Diego <diegosmtattoo@gmail.com>
2025-07-24 11:57:18 -0300

    feat: form navigation

--- src/components/InscriptionForm/index.tsx
+++ src/components/InscriptionForm/index.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { use } from "react";
 import { CloseButton, InscriptionContainer } from "./styles";
 import { useInscriptionStore } from "@/store/store";
+import { useRouter } from "next/navigation";
 
 const InscriptionForm: React.FC = () => {
   const { isFormVisible, isActive, hideForm } = useInscriptionStore();
+  const router = useRouter();
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push("/ceo-masterclass");
+    hideForm();
+  };
+
   return (
     <>
       {isFormVisible && (
@@ -20,7 +29,9 @@ const InscriptionForm: React.FC = () => {
             <input type="text" placeholder="Nome" />
             <input type="email" placeholder="Email" />
             <input type="text" placeholder="Celular" />
-            <button>Inscrição</button>
+            <button type="submit" onClick={handleFormSubmit}>
+              Inscrição
+            </button>
           </form>
           <div onClick={hideForm} className="overlay"></div>
         </InscriptionContainer>
