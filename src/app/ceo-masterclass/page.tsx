4169f43f23916fb209ad1c2c13a184c43993762e
Diego <diegosmtattoo@gmail.com>
2025-07-28 09:41:38 -0300

    fix: saved conten for sair dessa

--- src/app/ceo-masterclass/page.tsx
+++ src/app/ceo-masterclass/page.tsx
@@ -1,9 +1,9 @@
-import ASolucao from '@/sections/ASolucao'
-import QuemNaoEVisto from '@/sections/QuemNaoEVisto'
-import Apresentacao from '@/sections/Apresentacao'
-import Pacotes from '@/sections/Pacotes'
-import Footer from '@/components/Footer'
-import Checklist from '@/sections/Checklist'
+import ASolucao from "@/sections/ASolucao";
+import QuemNaoEVisto from "@/sections/QuemNaoEVisto";
+import Apresentacao from "@/sections/Apresentacao";
+import Pacotes from "@/sections/Pacotes";
+import Footer from "@/components/Footer";
+import Checklist from "@/sections/Checklist";
 
 export default function Home() {
   return (
@@ -15,5 +15,5 @@ export default function Home() {
       <Pacotes />
       <Footer />
     </div>
-  )
+  );
 }
